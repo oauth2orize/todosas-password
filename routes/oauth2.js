78f8539e31ab1203f3b8d3d64b65b4865a09000f
Jared Hanson <jaredhanson@gmail.com>
2022-08-25 09:39:59 -0700

    Verify password and issue access token.

--- routes/oauth2.js
+++ routes/oauth2.js
@@ -4,6 +4,7 @@ var passport = require('passport');
 var HTTPBasicStrategy = require('passport-http').BasicStrategy;
 var OAuth2ClientPasswordStrategy = require('passport-oauth2-client-password');
 var crypto = require('crypto');
+var dateFormat = require('dateformat');
 var db = require('../db');
 
 
@@ -36,8 +37,36 @@ as.exchange(oauth2orize.exchange.password(function issue(client, username, passw
   console.log(password);
   console.log(scope)
   
-  
-  return cb(null, 'at', 'rt');
+  db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) { return cb(null, false); }
+    
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+      if (err) { return cb(err); }
+      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+        return cb(null, false);
+      }
+      
+      crypto.randomBytes(64, function(err, buffer) {
+        if (err) { return cb(err); }
+        var accessToken = buffer.toString('base64');
+        var expiresAt = new Date(Date.now() + 3600000); // 1 hour from now
+      
+        db.run('INSERT INTO access_tokens (user_id, client_id, scope, expires_at, token) VALUES (?, ?, ?, ?, ?)', [
+          row.id,
+          '1', //row.client_id, // FIXME: add a proper client id here with public client auth
+          scope,
+          dateFormat(expiresAt, 'yyyy-mm-dd HH:MM:ss', true),
+          accessToken,
+        ], function(err) {
+          if (err) { return cb(err); }
+      
+      
+          return cb(null, accessToken, 'rt');
+        });
+      });
+    });
+  });
 }));
 
 var router = express.Router();
