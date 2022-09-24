f5a118b83112cb54f4e7688eb8dd4014138b0384
Jared Hanson <jaredhanson@gmail.com>
2022-09-24 08:14:15 -0700

    Split into users and authz DB.

--- db.js
+++ db.js
@@ -4,10 +4,11 @@ var crypto = require('crypto');
 
 mkdirp.sync('./var/db');
 
-var db = new sqlite3.Database('./var/db/users.db');
+var usersDB = new sqlite3.Database('./var/db/users.db');
+var authzDB = new sqlite3.Database('./var/db/authz.db');
 
-db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS users ( \
+usersDB.serialize(function() {
+  usersDB.run("CREATE TABLE IF NOT EXISTS users ( \
     id INTEGER PRIMARY KEY, \
     username TEXT UNIQUE, \
     hashed_password BLOB, \
@@ -15,14 +16,24 @@ db.serialize(function() {
     name TEXT \
   )");
   
-  db.run("CREATE TABLE IF NOT EXISTS clients ( \
+  // create an initial user (username: alice, password: letmein)
+  var salt = crypto.randomBytes(16);
+  usersDB.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+    'alice',
+    crypto.pbkdf2Sync('letmein', salt, 310000, 32, 'sha256'),
+    salt
+  ]);
+});
+
+authzDB.serialize(function() {
+  authzDB.run("CREATE TABLE IF NOT EXISTS clients ( \
     id INTEGER PRIMARY KEY, \
     secret TEXT, \
     name TEXT NOT NULL, \
     redirect_uri TEXT \
   )");
   
-  db.run("CREATE TABLE IF NOT EXISTS grants ( \
+  authzDB.run("CREATE TABLE IF NOT EXISTS grants ( \
     id INTEGER PRIMARY KEY, \
     user_id INTEGER NOT NULL, \
     client_id INTEGER NOT NULL, \
@@ -31,7 +42,7 @@ db.serialize(function() {
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP \
   )");
   
-  db.run("CREATE TABLE IF NOT EXISTS authorization_codes ( \
+  authzDB.run("CREATE TABLE IF NOT EXISTS authorization_codes ( \
     client_id INTEGER NOT NULL, \
     redirect_uri TEXT, \
     user_id INTEGER NOT NULL, \
@@ -42,7 +53,7 @@ db.serialize(function() {
     code TEXT UNIQUE NOT NULL \
   )");
   
-  db.run("CREATE TABLE IF NOT EXISTS access_tokens ( \
+  authzDB.run("CREATE TABLE IF NOT EXISTS access_tokens ( \
     user_id INTEGER NOT NULL, \
     client_id INTEGER NOT NULL, \
     scope TEXT, \
@@ -51,34 +62,27 @@ db.serialize(function() {
     token TEXT UNIQUE NOT NULL \
   )");
   
-  db.run("CREATE TABLE IF NOT EXISTS refresh_tokens ( \
+  authzDB.run("CREATE TABLE IF NOT EXISTS refresh_tokens ( \
     grant_id INTEGER NOT NULL, \
     issued_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
     expires_at DATETIME, \
     token TEXT UNIQUE NOT NULL \
   )");
   
-  // create an initial user (username: alice, password: letmein)
-  var salt = crypto.randomBytes(16);
-  db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
-    'alice',
-    crypto.pbkdf2Sync('letmein', salt, 310000, 32, 'sha256'),
-    salt
-  ]);
-  
   // create an initial client (client ID: 1, client secret: 7Fjfp0ZBr1KtDRbnfVdmIw)
-  db.run('INSERT OR IGNORE INTO clients (id, secret, name, redirect_uri) VALUES (?, ?, ?, ?)', [
+  authzDB.run('INSERT OR IGNORE INTO clients (id, secret, name, redirect_uri) VALUES (?, ?, ?, ?)', [
     1,
     '7Fjfp0ZBr1KtDRbnfVdmIw',
     'Todos',
     'http://localhost:3000/oauth2/redirect'
   ]);
   
-  db.run('INSERT OR IGNORE INTO clients (id, name, redirect_uri) VALUES (?, ?, ?)', [
+  authzDB.run('INSERT OR IGNORE INTO clients (id, name, redirect_uri) VALUES (?, ?, ?)', [
     2,
     'Todos',
     'http://localhost:3000/'
   ]);
 });
 
-exports.usersDB = db;
+exports.usersDB = usersDB;
+exports.authzDB = authzDB;
